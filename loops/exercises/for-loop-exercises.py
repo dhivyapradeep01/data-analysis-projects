dd9b57dac90d57f07696b8f871ee7a7009ae5015
Dhivya Pradeep <144861226+dhivyapradeep01@users.noreply.github.com>
2024-08-20 21:55:01 -0500

    completed exercise

--- loops/exercises/for-loop-exercises.py
+++ loops/exercises/for-loop-exercises.py
@@ -4,5 +4,29 @@
     # c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
 
+print ('\n a. Print the numbers 0 - 20, one number per line.\n')
 
 
+for i in range(21):
+    print(i)
+
+print ('\n b. Print only the ODD values from 3 - 29, one number per line.\n')
+       
+for i in range(2,30):
+    if i % 2 != 0:
+         print(i)
+
+print ('\nc. Print the EVEN numbers 12 to -14 in descending order, one number per line.\n')
+
+for i in range(12, -15, -2):
+    print(i)
+
+
+print ('\nChallenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).\n')
+
+for i in range(50, 20, -1):
+    if i % 3 == 0:
+        print(i)
+        
+
+print ('\n\n')
